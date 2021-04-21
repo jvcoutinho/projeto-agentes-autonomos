b786c9a27b0d88aa0a153561da331b4f806d51be
jvcoutinho <joao.coutinho9@gmail.com>
2021-04-21 19:17:14 -0300

    Defining base defend

--- managers/MilitaryManager.py
+++ managers/MilitaryManager.py
@@ -1,11 +1,14 @@
 import sc2
 from sc2 import UnitTypeId
+from sc2.units import Units
 
 from abstracts.Manager import Manager
 
 
 # Define estratégias de combate
 class MilitaryManager(Manager):
+
+    DEFEND_THRESHOLD = 0.6
 
     def __init__(self, agent: sc2.BotAI):
         super().__init__(agent)
@@ -15,14 +18,17 @@ class MilitaryManager(Manager):
 
     async def update(self, iteration: int):
         # Training #
-        self.train_units_on_structure([UnitTypeId.STALKER, UnitTypeId.ZEALOT], UnitTypeId.GATEWAY)
+        self.train_units_on_structure([UnitTypeId.DARKTEMPLAR, UnitTypeId.STALKER, UnitTypeId.ZEALOT], UnitTypeId.GATEWAY)
         self.train_units_on_structure([UnitTypeId.VOIDRAY], UnitTypeId.STARGATE)
-        self.train_units_on_structure([UnitTypeId.DARKTEMPLAR], UnitTypeId.DARKSHRINE)
 
         # Combat #
-        pass
+        await self.defend_base()
 
     def train_units_on_structure(self, unit_type_ids, structure_type_id):
+        """
+        Train an array of units in a structure who trains them. Due to the
+        resources, units at the start of the array should be more costly.
+        """
         for unit_id in unit_type_ids:
             for structure in self.agent.structures(structure_type_id).ready.idle:
                 if self.agent.can_afford(unit_id):
@@ -35,3 +41,33 @@ class MilitaryManager(Manager):
         if not self.agent.townhalls.ready:
             for worker in self.agent.workers:
                 worker.attack(self.agent.enemy_start_locations[0])
+
+    async def defend_base(self):
+        """
+        If there are enemies too close to a Nexus, then defend if there are enough units. Otherwise, flee to another Nexus.
+        """
+        for townhall in self.agent.townhalls:
+            close_enemies = self.enemies().closer_than(15, townhall)
+            if not close_enemies.exists:
+                continue
+
+            close_combatents = self.combatents().closer_than(40, townhall)
+            if close_combatents.amount >= close_enemies.amount * self.DEFEND_THRESHOLD:
+                for combatent in close_combatents:
+                    combatent.attack(close_enemies.closest_to(combatent))
+            # elif self.agent.townhalls.amount > 1:
+            #     neighbour_townhall = self.agent.townhalls.filter(lambda t: t != townhall).closest_to(townhall)
+            #     for combatent in close_combatents:
+            #         combatent.move(neighbour_townhall)
+
+    def combatents(self) -> Units:
+        return (
+            self.agent.units(UnitTypeId.VOIDRAY) |
+            self.agent.units(UnitTypeId.ZEALOT) |
+            self.agent.units(UnitTypeId.DARKTEMPLAR) |
+            self.agent.units(UnitTypeId.STALKER)
+        )
+
+    def enemies(self) -> Units:
+        return (self.agent.enemy_units | self.agent.enemy_structures).filter(lambda t: t.can_be_attacked)
+
--- managers/StructureManager.py
+++ managers/StructureManager.py
@@ -6,12 +6,16 @@ from abstracts.Manager import Manager
 
 # Define criação e funcionamento de estruturas
 class StructureManager(Manager):
-    MAXIMUM_NUMBER_TOWNHALLS = 3
+    MAXIMUM_NUMBER_TOWNHALLS = 6
     MAXIMUM_NUMBER_ASSIMILATORS = 3
     MAXIMUM_NUMBER_PYLONS = 10
     MAXIMUM_NUMBER_FORGES = 2
-    MAXIMUM_NUMBER_GATEWAYS = 9
+    MAXIMUM_NUMBER_GATEWAYS = 2
     MAXIMUM_NUMBER_CYBERNETICSCORE = 1
+    MAXIMUM_NUMBER_STARGATES = 1
+    MAXIMUM_NUMBER_PHOTON_CANNONS = 5
+    MAXIMUM_NUMBER_TWILIGHT_COUNCILS = 1
+    MAXIMUM_NUMBER_DARK_SHRINES = 1
     SUPPLY_THRESHOLD_FOR_PYLON = 5
 
     def __init__(self, agent: sc2.BotAI):
@@ -21,19 +25,23 @@ class StructureManager(Manager):
         pass
 
     async def update(self, iteration: int):
-        await self.build_pylons()
-        await self.build_gas_buildings()
         await self.build_townhalls()
+        await self.build_pylons()
         await self.build_forge()
         await self.build_gateway()
         await self.build_cybernetics_core()
+        await self.build_stargate()
+        await self.build_gas_buildings()
+        await self.build_photon_cannon()
+        # await self.build_twilight_council()
+        # await self.build_dark_shrine()
 
     async def build_townhalls(self):
         """
         Create another Nexus whenever possible, up until a limit.
         """
         if (
-            self.agent.structures(UnitTypeId.NEXUS).amount < self.MAXIMUM_NUMBER_TOWNHALLS
+            self.count_with_pending_structure(UnitTypeId.NEXUS) < self.MAXIMUM_NUMBER_TOWNHALLS
             and self.agent.can_afford(UnitTypeId.NEXUS)
         ):
             await self.agent.expand_now()
@@ -102,14 +110,40 @@ class StructureManager(Manager):
             await self.build_structure_near_random_pylon(UnitTypeId.CYBERNETICSCORE,
                                                          self.MAXIMUM_NUMBER_CYBERNETICSCORE)
 
-    async def build_structure_near_random_pylon(self, unit_type_id, limit: int):
+    async def build_stargate(self):
+        """
+        Create a Stargate whenever possible near a random Pylon, up until a limit.
+        """
+        await self.build_structure_near_random_pylon(UnitTypeId.STARGATE, self.MAXIMUM_NUMBER_STARGATES)
+
+    async def build_twilight_council(self):
+        """
+        Create a Twilight Council whenever possible near a random Pylon, up until a limit.
+        """
+        await self.build_structure_near_random_pylon(UnitTypeId.TWILIGHTCOUNCIL, self.MAXIMUM_NUMBER_TWILIGHT_COUNCILS)
+
+    async def build_dark_shrine(self):
+        """
+        Create a Dark Shrine whenever possible near a random Pylon, up until a limit.
+        """
+        await self.build_structure_near_random_pylon(UnitTypeId.DARKSHRINE, self.MAXIMUM_NUMBER_DARK_SHRINES)
+
+    async def build_photon_cannon(self):
+        """
+        Create a Photon Cannon whenever possible near a random Pylon, up until a limit.
+        """
+        await self.build_structure_near_random_pylon(UnitTypeId.PHOTONCANNON, self.MAXIMUM_NUMBER_PHOTON_CANNONS)
+
+    async def build_structure_near_random_pylon(self, unit_type_id, limit: int, placement_step: int = 2):
         pylons = self.agent.structures(UnitTypeId.PYLON).ready
         if (
             pylons.exists
             and self.count_with_pending_structure(unit_type_id) < limit
             and self.agent.can_afford(unit_type_id)
         ):
-            await self.agent.build(unit_type_id, near=pylons.random)
+            await self.agent.build(unit_type_id, near=pylons.random, placement_step=placement_step)
 
     def count_with_pending_structure(self, unit_type_id) -> int:
         return self.agent.structures(unit_type_id).amount + self.agent.already_pending(unit_type_id)
+
+
